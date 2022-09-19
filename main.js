5291ed4d5de1c483d14d265eb58084ac0856969e
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2022-09-19 19:26:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function setup(){
+  video = createCapture(VIDEO);
+  video.size(550,500);
+  video.position(150,150)
+
+ var canvas = createCanvas(550,550);
+ canvas.position(850,150);
+ 
+ poseNet = ml5.poseNet(video,modelLoaded);
+ poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet is initialised");
+}
+function draw(){
+ background("#2A9B00");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
